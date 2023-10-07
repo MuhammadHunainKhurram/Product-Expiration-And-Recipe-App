27430ea358dcb40fb74f20c8de47a12c994a36a2
Muhammad Khurram <muhammad.khurram752@gmail.com>
2023-10-07 14:15:56 -0500

    Update

--- Backend/cookme.py
+++ Backend/cookme.py
@@ -1,1 +1,11 @@
-llkn
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)

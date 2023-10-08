d40bf5ffef010ad7caec9328845bb74fe1a30b64
Jack Youssef <jackyoussef@icloud.com>
2023-10-07 22:57:55 -0500

    Update cookme.py

--- Backend/cookme.py
+++ Backend/cookme.py
@@ -1,5 +1,6 @@
 from barcode_reader import read_barcode
 from nutrition import fetch_nutrition
+from recipe import maker
 from flask import Flask, render_template, request, redirect, url_for
 from flask_cors import CORS, cross_origin
 import json
@@ -29,7 +30,7 @@ def save_ingredient():
 
     return redirect(url_for('index'))
 
-
+# takes an image, stores product info, returns info
 @app.route('/barcode', methods=['PUT'])
 def get_food():
     if request.method == 'PUT':
@@ -45,6 +46,19 @@ def get_food():
             json.dump(data, json_file, indent=4)
         
         return nutrition
+    
+# takes list of items and returns recipes
+@app.route('/recipe', methods=['PUT'])
+def get_recipe():
+    if request.method == 'PUT':
+    
+        with open('Backend/db/data.json', 'r') as db:
+            data = json.load(db)
+
+        product_names = [entry['product']['Name'] for entry in data['data']]
+        recipe = maker(product_names)
+        
+        return recipe
 
 
 if __name__ == "__main__":
